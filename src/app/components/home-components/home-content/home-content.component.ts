ffbd8a6d4517eca21d28bd2703f02e10aec989bd
ib4717 <ib4717@srmist.edu.in>
2022-07-30 18:39:56 +0530

    updated

--- src/app/components/home-components/home-content/home-content.component.ts
+++ src/app/components/home-components/home-content/home-content.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeContentComponent implements OnInit {
 
+  imageIndex: number = -1
+  numberOfImages: number = 3
+  bannerImage: string = ''
+  timeForBannerToSwitch: number = 3000 // in milli seconds
+
+  bannerAutoSwitchInterval: any
+
+  bannerImages = [
+    '../../../../assets/images/wm.webp', 
+    '../../../../assets/images/women-empowerment.jpeg',
+    '../../../../assets/images/women-empowerment.webp']
+
   constructor() { }
 
   ngOnInit(): void {
+    this.nextImage()
+
+    this.bannerAutoSwitchInterval = setInterval(() => {
+      this.autoNextImage()
+    }, this.timeForBannerToSwitch)
+  }
+
+  previousImage(): void {
+    if(this.bannerAutoSwitchInterval)
+      clearInterval(this.bannerAutoSwitchInterval)
+    if(this.imageIndex == 0)
+      this.imageIndex = this.numberOfImages - 1
+    else this.imageIndex = this.imageIndex - 1
+    this.imageIndex = this.imageIndex%this.numberOfImages
+    this.bannerImage = this.bannerImages[this.imageIndex]
+  }
+
+  nextImage(): void {
+    if(this.bannerAutoSwitchInterval)
+      clearInterval(this.bannerAutoSwitchInterval)
+    this.imageIndex = this.imageIndex + 1
+    this.imageIndex = this.imageIndex%this.numberOfImages
+    this.bannerImage = this.bannerImages[this.imageIndex]
+  }
+
+  autoNextImage(): void {
+    this.imageIndex = this.imageIndex + 1
+    this.imageIndex = this.imageIndex%this.numberOfImages
+    this.bannerImage = this.bannerImages[this.imageIndex]
   }
 
 }
