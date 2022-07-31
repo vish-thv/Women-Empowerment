d6773751efac833f749d34cdc2aabddd465473c1
ib4717 <ib4717@srmist.edu.in>
2022-07-31 18:16:36 +0530

    updated

---
+++ src/app/models/trainee/trainee-personal-details.ts
@@ -0,0 +1,11 @@
+export interface TraineePersonalDetails {
+    traineeId: string
+    emailId: string
+    aadhaar: string
+    pan: string
+    maritalStatus: string
+    religion: string
+    category: string
+    personWithDisability: boolean
+    disabilityType: string
+}
